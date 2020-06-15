e2af1a3bc4038b39e4610c74b36e41900758d678
Thivi <theviyanthan@outlook.com>
2020-06-15 19:02:31 +0530

    Add custom grant message

--- oauth-web-worker/src/constants/messages.ts
+++ oauth-web-worker/src/constants/messages.ts
@@ -22,5 +22,5 @@ export const SIGNED_IN = "signed-in";
 export const AUTH_REQUIRED = "auth-required";
 export const AUTH_CODE = "auth-code";
 export const LOGOUT = "logout";
-export const SWITCH_ACCOUNTS = "switch-accounts";
 export const API_CALL = "api-call";
+export const CUSTOM_GRANT = "custom-grant";
