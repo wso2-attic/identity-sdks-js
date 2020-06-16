e1f5758054d67fc2cce5eef4a39fbd2daa9f3314
Thivi <theviyanthan@outlook.com>
2020-06-16 17:55:26 +0530

    Update eslint config

--- oauth-web-worker/.eslintrc.js
+++ oauth-web-worker/.eslintrc.js
@@ -1,112 +1,107 @@
 module.exports = {
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:import/typescript",
-        "plugin:react-hooks/recommended"
-    ],
-    parserOptions: {
-        ecmaVersion: 9,
-        sourceType: "module"
-    },
-    plugins: ["import"],
-    settings: {
-        react: {
-            version: "detect"
-        }
-    },
-    env: {
-        browser: true,
-        jest: true,
-        node: true,
-        es6: true
-    },
-    rules: {
-        "eol-last": "error",
-        "quotes": ["warn", "double"],
-        "max-len": ["warn", { "code": 120 }],
-        "comma-dangle": ["warn", "never"],
-        "sort-imports": ["warn", {
-            "ignoreCase": false,
-            "ignoreDeclarationSort": true,
-            "ignoreMemberSort": false
-        }],
-        "import/order": [
-            "warn",
-            {
-                "groups": [ "builtin", "external", "index", "sibling", "parent", "internal" ],
-                "alphabetize": {
-                    order: 'asc',
-                    caseInsensitive: true
-                }
-            }
-        ],
-        "react/jsx-curly-spacing": [
-            "warn",
-            {
-                when: "never",
-                children: { "when": "always" },
-                allowMultiline: true,
-                spacing: { objectLiterals: "always" }
-            }
-        ],
-        "react/no-children-prop": 0,
-        "sort-keys": ["warn", "asc", {"caseSensitive": true, "natural": false, "minKeys": 2}],
-        "object-curly-spacing": ["warn", "always"],
-        "no-console": "warn",
-        "no-duplicate-imports": "warn",
-        "no-restricted-imports": ["warn", { "patterns": ["@wso2is/**/dist/**"] }]
-    },
-    overrides: [
-        {
-            files: ["**/*.tsx", "**/*.ts"],
-            parser: "@typescript-eslint/parser",
-            extends: [
-                "eslint:recommended",
-                "plugin:react/recommended",
-                "plugin:@typescript-eslint/eslint-recommended",
-                "plugin:@typescript-eslint/recommended"
-            ],
-            parserOptions: {
-                ecmaVersion: 9,
-                sourceType: "module"
-            },
-            settings: {
-                react: {
-                    version: "detect"
-                }
-            },
-            env: {
-                browser: true,
-                node: true,
-                es6: true
-            },
-            rules: {
-                "eol-last": "error",
-                "react/jsx-curly-spacing": [
-                    2,
-                    "always",
-                    {
-                        allowMultiline: true,
-                        spacing: { objectLiterals: "always" }
-                    }
-                ],
-                "react/no-children-prop": 0,
-                "react/prop-types": "off",
-                "@typescript-eslint/no-explicit-any": 0,
-                "@typescript-eslint/explicit-function-return-type": 0,
-                "react/display-name": 0,
-                "no-use-before-define": "off",
-                "@typescript-eslint/no-use-before-define": [
-                    "warn",
-                    {
-                        functions: false,
-                        classes: false,
-                        variables: false,
-                        typedefs: false
-                    }
-                ]
-            }
-        }
-    ]
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true
+	},
+	extends: ["eslint:recommended", "plugin:import/typescript"],
+	overrides: [
+		{
+			env: {
+				browser: true,
+				es6: true,
+				node: true
+			},
+			extends: [
+				"eslint:recommended",
+				"plugin:@typescript-eslint/eslint-recommended",
+				"plugin:@typescript-eslint/recommended"
+			],
+			files: ["**/*.tsx", "**/*.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				ecmaVersion: 9,
+				sourceType: "module"
+			},
+			rules: {
+				"@typescript-eslint/explicit-function-return-type": 0,
+				"@typescript-eslint/no-explicit-any": 0,
+				"@typescript-eslint/no-inferrable-types": "off",
+				"@typescript-eslint/no-use-before-define": [
+					"warn",
+					{
+						classes: false,
+						functions: false,
+						typedefs: false,
+						variables: false
+					}
+				],
+				"eol-last": "error",
+				"no-use-before-define": "off"
+			},
+			settings: {
+				react: {
+					version: "detect"
+				}
+			}
+		}
+	],
+	parserOptions: {
+		ecmaVersion: 9,
+		sourceType: "module"
+	},
+	plugins: ["import"],
+	rules: {
+		"comma-dangle": ["warn", "never"],
+		"eol-last": "error",
+		"import/order": [
+			"warn",
+			{
+				alphabetize: {
+					caseInsensitive: true,
+					order: "asc"
+				},
+				groups: ["builtin", "external", "index", "sibling", "parent", "internal"]
+			}
+		],
+		"max-len": [
+			"warn",
+			{
+				code: 120
+			}
+		],
+		"no-console": "warn",
+		"no-duplicate-imports": "warn",
+		"no-restricted-imports": [
+			"warn",
+			{
+				patterns: ["@wso2is/**/dist/**"]
+			}
+		],
+		"object-curly-spacing": ["warn", "always"],
+		quotes: ["warn", "double"],
+		"sort-imports": [
+			"warn",
+			{
+				ignoreCase: false,
+				ignoreDeclarationSort: true,
+				ignoreMemberSort: false
+			}
+		],
+		"sort-keys": [
+			"warn",
+			"asc",
+			{
+				caseSensitive: true,
+				minKeys: 2,
+				natural: false
+			}
+		]
+	},
+	settings: {
+		react: {
+			version: "detect"
+		}
+	}
 };
